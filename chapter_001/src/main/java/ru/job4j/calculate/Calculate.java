3d33de5504fa4fdde288c6167105087338a59a9b
Oleg Kaschuk <okaschuk@gmail.com>
2019-11-03 00:06:12 +0400

    5.8. JavaDoc. Code convention[#193762]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,23 +1,52 @@
-package ru.job4j.calculator;
+package ru.job4j.calculate;
 
-public class Calculator {
-
+/**
+ * Calculate
+ * @author Oleg Kaschuk (okaschuk@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Calculate {
+    /**
+     * Calculate adding
+     * @param first  first item
+     * @param second  second item
+     */
     public static void add(double first, double second) {
         System.out.println(first + " + " + second + " = " + (first + second));
     }
 
+    /**
+     * Calculate subtract
+     * @param first  first item
+     * @param second second item
+     */
     public static void subtract(double first, double second) {
         System.out.println(first + " - " + second + " = " + (first - second));
     }
 
+    /**
+     * Calculate multiply
+     * @param first first item
+     * @param second second item
+     */
     public static void multiply(double first, double second) {
         System.out.println(first + " * " + second + " = " + (first * second));
     }
 
+    /**
+     * Calculate division
+     * @param first first item
+     * @param second second item
+     */
     public static void div(double first, double second) {
         System.out.println(first + " / " + second + " = " + (first / second));
     }
 
+    /**
+     * Main
+     * @param args args
+     */
     public static void main(String[] args) {
         add(1, 1);
         div(4, 2);
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for calculate task.
+ *
+ * @author Oleg Kaschuk (okaschuk@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+package ru.job4j.calculate;
