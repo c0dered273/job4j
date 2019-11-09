09269704329749600ac32efb88ba6cdaf985c65e
Oleg Kaschuk <okaschuk@gmail.com>
2019-11-09 20:07:34 +0400

    2. Чтение кода и базовые ошибки.[#193863]

--- chapter_001/src/main/java/ru/job4j/condition/SqArea.java
+++ chapter_001/src/main/java/ru/job4j/condition/SqArea.java
@@ -3,7 +3,6 @@ package ru.job4j.condition;
 public class SqArea {
 
     public static double square(int p, int k) {
-//        return Math.pow(((double) p / (2*k + 2)), 2) * k;
         int h = p / (2 * (k + 1));
         int w = k * h;
         int s = w * h;
