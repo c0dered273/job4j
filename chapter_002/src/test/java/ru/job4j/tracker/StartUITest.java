12cde85e2e1023a734ec0a466b3f7450ea18b567
Oleg Kaschuk <okaschuk@gmail.com>
2019-12-09 17:14:39 +0400

    9. Написать тесты на StartUI.[#193871]

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,43 +1,21 @@
 package ru.job4j.tracker;
 
-import org.hamcrest.core.IsNull;
 import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class StartUITest {
-//    @Test
-//    public void whenAddItem() {
-//        String[] answers = {"Fix PC"};
-//        Input input = new StubInput(answers);
-//        Tracker tracker = new Tracker();
-//        StartUI.createItem(input, tracker);
-//        Item created = tracker.findAll()[0];
-//        Item expected = new Item("Fix PC");
-//        assertThat(created.getName(), is(expected.getName()));
-//    }
-//
-//    @Test
-//    public void whenReplaceItem() {
-//        Tracker tracker = new Tracker();
-//        Item item = new Item("new item");
-//        tracker.add(item);
-//        String[] answers = {
-//                item.getId(),
-//                "replaced item"
-//        };
-//        StartUI.replaceItem(new StubInput(answers), tracker);
-//        Item replaced = tracker.findById(item.getId());
-//        assertThat(replaced.getName(), is("replaced item"));
-//    }
-//
-//    @Test
-//    public void whenDeleteItem() {
-//        Tracker tracker = new Tracker();
-//        Item item = tracker.add(new Item("item to delete"));
-//        String delItemId = item.getId();
-//        String[] answers = {delItemId};
-//        StartUI.deleteItem(new StubInput(answers), tracker);
-//        assertThat(tracker.findById(delItemId), is(IsNull.nullValue()));
-//    }
+    @Test
+    public void whenExit() {
+        StubInput input = new StubInput(
+                new String[] {"2"}
+        );
+        StubAction action = new StubAction();
+        new StartUI().init(input, new Tracker(), new UserAction[] { new StubAction(),
+                new StubAction(),
+                action,
+                new StubAction(),
+        });
+        assertThat(action.isCall(), is(true));
+    }
 }
