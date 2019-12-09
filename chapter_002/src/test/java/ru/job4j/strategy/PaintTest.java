476637206438344e0d8e8ab0164e420d4f1328e1
Oleg Kaschuk <okaschuk@gmail.com>
2019-12-09 17:24:34 +0400

    9.1 Рефакторинг теста @Before @After[#193867]

--- chapter_002/src/test/java/ru/job4j/strategy/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/strategy/PaintTest.java
@@ -3,18 +3,31 @@ package ru.job4j.strategy;
 import java.io.PrintStream;
 import java.io.ByteArrayOutputStream;
 import org.junit.Test;
+import org.junit.After;
+import org.junit.Before;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class PaintTest {
+    private final PrintStream stdout = System.out;
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.out.println("execute before method");
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+        System.out.println("execute after method");
+    }
 
     @Test
     public void whenDrawSquare() {
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().draw(new Square());
-        assertThat(new String(out.toByteArray()), is(new StringBuilder()
+        assertThat(new String(this.out.toByteArray()), is(new StringBuilder()
                         .append("******")
                         .append(System.lineSeparator())
                         .append("******")
@@ -30,16 +43,12 @@ public class PaintTest {
                         .toString()
                 )
         );
-        System.setOut(stdout);
     }
 
     @Test
     public void whenDrawTriangle() {
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().draw(new Triangle());
-        assertThat(new String(out.toByteArray()), is(new StringBuilder()
+        assertThat(new String(this.out.toByteArray()), is(new StringBuilder()
                         .append("   *   ")
                         .append(System.lineSeparator())
                         .append("  ***  ")
@@ -51,6 +60,5 @@ public class PaintTest {
                         .toString()
                 )
         );
-        System.setOut(stdout);
     }
 }
