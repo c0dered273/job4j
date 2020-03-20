2df4444733b43ac0ca793fd8d59fb920e13bef87
Oleg Kaschuk <okaschuk@gmail.com>
2020-03-20 15:55:33 +0400

    3. Уникальность элементов и сортировка.[#193724]

--- chapter_002/src/main/java/ru/job4j/turist/Profiles.java
+++ chapter_002/src/main/java/ru/job4j/turist/Profiles.java
@@ -1,12 +1,16 @@
 package ru.job4j.turist;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Profiles {
     public static List<Address> collect(List<Profile> profiles) {
+        Comparator<Address> comp = Comparator.comparing(Address::getCity);
         return profiles.stream()
                 .map(Profile::getAddress)
+                .sorted(comp)
+                .distinct()
                 .collect(Collectors.toList());
     }
 }
--- chapter_002/src/test/java/ru/job4j/turist/ProfilesTest.java
+++ chapter_002/src/test/java/ru/job4j/turist/ProfilesTest.java
@@ -13,17 +13,19 @@ public class ProfilesTest {
     private Address addr1 = new Address("City1", "street1", 5, 34);
     private Address addr2 = new Address("City2", "street2", 41, 842);
     private Address addr3 = new Address("City3", "street3", 109, 4);
+    private Address addr4 = new Address("City2", "street2", 41, 842);
     List<Profile> profilesList = new ArrayList<>();
 
     @Before
     public void prepareData() {
+        profilesList.add(new Profile(addr3));
         profilesList.add(new Profile(addr1));
         profilesList.add(new Profile(addr2));
-        profilesList.add(new Profile(addr3));
+        profilesList.add(new Profile(addr4));
     }
 
     @Test
-    public void whenGetAddresses() {
+    public void whenGetSortedAddresses() {
         List<Address> expect = new ArrayList<>();
         expect.add(addr1);
         expect.add(addr2);
