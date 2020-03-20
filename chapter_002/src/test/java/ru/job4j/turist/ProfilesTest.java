1673bb18edbc151f343513a141ec276dc699cb40
Oleg Kaschuk <okaschuk@gmail.com>
2020-03-20 15:08:40 +0400

    2. Список адресов[#193723]

---
+++ chapter_002/src/test/java/ru/job4j/turist/ProfilesTest.java
@@ -0,0 +1,34 @@
+package ru.job4j.turist;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThat;
+
+public class ProfilesTest {
+    private Address addr1 = new Address("City1", "street1", 5, 34);
+    private Address addr2 = new Address("City2", "street2", 41, 842);
+    private Address addr3 = new Address("City3", "street3", 109, 4);
+    List<Profile> profilesList = new ArrayList<>();
+
+    @Before
+    public void prepareData() {
+        profilesList.add(new Profile(addr1));
+        profilesList.add(new Profile(addr2));
+        profilesList.add(new Profile(addr3));
+    }
+
+    @Test
+    public void whenGetAddresses() {
+        List<Address> expect = new ArrayList<>();
+        expect.add(addr1);
+        expect.add(addr2);
+        expect.add(addr3);
+        List<Address> result = Profiles.collect(profilesList);
+        assertThat(expect, is(result));
+    }
+}
